c7acc9d673d43a3f694a32bc54448854df7f615f
Bruno Tagliapietra <bruno.tagliapietra@xero.com>
2017-07-31 11:16:19 +1200

    service offline error message

--- src/scripts/sendmsg.js
+++ src/scripts/sendmsg.js
@@ -1,6 +1,8 @@
 'use strict';
 
 function sendMessage() {
+    document.getElementById("error").style.visibility = "hidden";
+
     var xhr = new XMLHttpRequest();
 
     var message = getValueOrPlaceholder("messageField");
@@ -13,7 +15,12 @@ function sendMessage() {
     }
 
     xhr.open("POST", "https://zc88ml120c.execute-api.us-east-1.amazonaws.com/dev/send",false);
-    xhr.send(JSON.stringify(payload))
+    try{
+        xhr.send(JSON.stringify(payload))
+    }catch(e){
+        console.error(e);
+        document.getElementById("error").style.visibility = "visible";
+    }
 }
 
 function getValueOrPlaceholder(id){
